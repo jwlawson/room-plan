27d7638b3c7f9e5cbbb3c8dfde42a760d93d388a
John Lawson <john@jwlawson.co.uk>
2012-06-17 11:37:13 +0100

    Added toString method to help debugging
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Plan/src/uk/co/jwlawson/plan/entities/Line.java
+++ Plan/src/uk/co/jwlawson/plan/entities/Line.java
@@ -16,35 +16,42 @@ import android.graphics.PointF;
 import android.util.Log;
 
 public class Line {
-	
+
 	private final static String TAG = "Line";
 	private static final boolean DEBUG = false;
-	
+
 	private PointF start;
 	private PointF end;
-	private Paint mPaint;
-	
+	private final Paint mPaint;
+
 	public Line(float x1, float y1, float x2, float y2, Paint paint) {
 		start = new PointF(x1, y1);
 		end = new PointF(x2, y2);
-		
+
 		mPaint = paint;
-		
-		if (DEBUG) Log.d(TAG, "New " + TAG + " created.");
+
+		if (DEBUG)
+			Log.d(TAG, "New " + TAG + " created.");
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Line from ( " + start.x + " , " + start.y + " ) to ( " + end.x + " , " + end.y
+				+ " )";
+	}
+
 	public void draw(Canvas canvas) {
 		canvas.drawLine(start.x, start.y, end.x, end.y, mPaint);
 	}
-	
+
 	public PointF getStart() {
 		return start;
 	}
-	
+
 	public PointF getEnd() {
 		return end;
 	}
-	
+
 	public void reverse() {
 		PointF tmp = start;
 		start = end;
