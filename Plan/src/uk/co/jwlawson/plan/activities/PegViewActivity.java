0533f27410c161f87beb99c9a888f0ea4765d5d0
John Lawson <john@jwlawson.co.uk>
2012-06-17 11:37:36 +0100

    Fixed done and reset methods
    Signed-off-by: John Lawson <john@jwlawson.co.uk>

--- Plan/src/uk/co/jwlawson/plan/activities/PegViewActivity.java
+++ Plan/src/uk/co/jwlawson/plan/activities/PegViewActivity.java
@@ -10,11 +10,7 @@
  *******************************************************************************/
 package uk.co.jwlawson.plan.activities;
 
-import java.util.ArrayList;
-
-import uk.co.jwlawson.plan.R;
-import uk.co.jwlawson.plan.entities.Line;
-import uk.co.jwlawson.plan.views.PegView;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -24,77 +20,112 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
+import uk.co.jwlawson.plan.R;
+import uk.co.jwlawson.plan.entities.Line;
+import uk.co.jwlawson.plan.views.PegView;
+
+import java.util.ArrayList;
+
 /** Activity to show and manage the peg board room creation. */
 public class PegViewActivity extends SherlockActivity {
-	
+
 	private static final String TAG = "PegViewActivity";
 	private static final boolean DEBUG = true;
-	
+
 	private PegView mPegView;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.peg_view_layout);
-		
+
 		mPegView = (PegView) findViewById(R.id.pegView1);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inf = getSupportMenuInflater();
 		inf.inflate(R.menu.peg_view_act_menu, menu);
 		if (DEBUG) Log.d(TAG, "Menu inflated");
 		return true;
-		
+
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.peg_view_menu_done) {
+		switch (item.getItemId()) {
+		case R.id.peg_view_menu_done:
 			done();
 			if (DEBUG) Log.d(TAG, "Menu item DONE selected");
+			break;
+		case R.id.peg_view_menu_reset:
+			reset();
+			if (DEBUG) Log.d(TAG, "Menu item RESET selected");
+			break;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 		return true;
 	}
-	
+
 	private void done() {
-		ArrayList<Line> lineList = mPegView.getLineList();
-		
+		ArrayList<Line> lineList = new ArrayList<Line>(mPegView.getLineList());
+
 		if (lineList.isEmpty()) {
 			Toast.makeText(getApplicationContext(), "No walls added", Toast.LENGTH_SHORT).show();
 			return;
 		}
-		
-		ArrayList<Line> result = new ArrayList<Line>();
-		
+
+		ArrayList<PointF> pointList = new ArrayList<PointF>();
+
 		Line l1 = lineList.get(0);
-		Line initLine = l1;
-		// result.add(l1);
-		
-		do {
-			for (Line l2 : lineList) {
-				if (l2.getStart().equals(l1.getEnd())) {
-					result.add(l2);
-					lineList.remove(l2);
-					l1 = l2;
+		lineList.remove(l1);
+
+		PointF initPoint = l1.getStart();
+		pointList.add(initPoint);
+		Log.d(TAG, l1.toString());
+		PointF start = l1.getEnd();
+		pointList.add(start);
+
+		boolean flag = false;
+
+		while (!initPoint.equals(start.x, start.y)) {
+
+			flag = false;
+
+			for (Line line : lineList) {
+
+				if (line.getStart().equals(start.x, start.y)) {
+					Log.d(TAG, line.toString());
+					pointList.add(line.getEnd());
+					start = line.getEnd();
+					lineList.remove(line);
+					flag = true;
 					break;
-				}
-				if (l2.getEnd().equals(l1.getEnd())) {
-					l2.reverse();
-					result.add(l2);
-					lineList.remove(l2);
-					l1 = l2;
+
+				} else if (line.getEnd().equals(start.x, start.y)) {
+					Log.d(TAG, line.toString());
+					pointList.add(line.getStart());
+					start = line.getStart();
+					lineList.remove(line);
+					flag = true;
 					break;
+
 				}
-				Toast.makeText(getApplicationContext(), "No walls added", Toast.LENGTH_SHORT).show();
-				return;
 			}
-		} while (l1 != initLine && !lineList.isEmpty());
-		
-		if (l1 != initLine) {
-			Toast.makeText(getApplicationContext(), "Room shape not valid", Toast.LENGTH_SHORT).show();
+			// No match found. Error!
+			if (!flag) {
+				Toast.makeText(getApplicationContext(), "Invalid room", Toast.LENGTH_SHORT).show();
+				return;
+			} else {
+				lineList.remove(start);
+			}
 		}
+
+	}
+
+	private void reset() {
+		mPegView.reset();
 	}
 }
